1a6021e4da53d11d492badf191fc9ac55a12e5e9
Ladow1 <vladimir.ksenic@itvkurze.sk>
2021-05-04 20:11:13 +0200

    cookie

--- myjs.js
+++ myjs.js
@@ -1,4 +1,5 @@
-"SameSite=None";
+> document.cookie = "promo_shown=1; Max-Age=2600000; Secure"
+< "promo_shown=1; Max-Age=2600000; Secure"
 
 /* <![CDATA[ */
 var tocplus = {"visibility_show":"show","visibility_hide":"hide","width":"Auto"};
