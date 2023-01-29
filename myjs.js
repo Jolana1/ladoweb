a8876596eed7aab330804ed27260be9e1a233e4f
Jolana1 <ksenicv@icloud.com>
2023-01-29 16:45:57 +0100

    oprava 29.1.23

--- myjs.js
+++ myjs.js
@@ -1,7 +1,9 @@
+promo_shown=1; SameSite=Lax
+/*promo_shown=1; Max-Age=2600000; Secure
 > document.cookie = "promo_shown=1; Max-Age=2600000; Secure"
-< "promo_shown=1; Max-Age=2600000; Secure"
+
 
 /* <![CDATA[ */
-var tocplus = {"visibility_show":"show","visibility_hide":"hide","width":"Auto"};
+/*var tocplus = {"visibility_show":"show","visibility_hide":"hide","width":"Auto"};
 /* ]]> */
-</script>
+
